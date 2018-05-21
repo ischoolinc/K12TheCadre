9c899206f32126f49d015e8316a484f61cb3b462
Grady_Liu <grady.liu@ischool.com.tw>
2018-05-21 15:03:12 +0800

    2018/05/21_羿均_新增國中功能代碼，避免模組合併後造成國中系統功能被disable。

--- K12.Behavior.TheCadre/Permissions.cs
+++ K12.Behavior.TheCadre/Permissions.cs
@@ -11,31 +11,31 @@ namespace K12.Behavior.TheCadre
     /// </summary>
     public static class Permissions
     {
-       // public static string 幹部記錄 { get { return "6B1F4596-D2C8-44E2-ADFD-D01E9E61B796"; } }
+       
         public static string 幹部記錄 { get { return "K12.Student.TheCadre.Detail00040"; } }
-        //public static string 幹部記錄_國中 { get { return "Behavior.TheCadre.Detail00040"; } }
+        public static string 幹部記錄_國中 { get { return "Behavior.TheCadre.Detail00040"; } }
 
         public static string 學生幹部證明單 { get { return "K12.Student.TheCadre.Report00060"; } }
-        //public static string 學生幹部證明單_國中 { get { return "Behavior.TheCadre.Report00060"; } }
+        public static string 學生幹部證明單_國中 { get { return "Behavior.TheCadre.Report00060"; } }
 
         public static string 班級幹部總表 { get { return "K12.class.TheCadre.Report00060.5"; } }
 
         public static string 學校幹部總表 { get { return "K12.class.TheCadre.Report00060.8"; } }
 
         public static string 班級幹部登錄 { get { return "K12.class.TheCadre.Report00070.1"; } }
-        //public static string 班級幹部登錄_國中 { get { return "Behavior.TheCadre.Report00070.1"; } }
+        public static string 班級幹部登錄_國中 { get { return "Behavior.TheCadre.Report00070.1"; } }
 
         public static string 匯出擔任幹部記錄 { get { return "K12.Student.Student.Ribbon0167"; } }
-        //public static string 匯出擔任幹部記錄_國中 { get { return "JHSchool.Student.Ribbon0167"; } }
+        public static string 匯出擔任幹部記錄_國中 { get { return "JHSchool.Student.Ribbon0167"; } }
 
         public static string 匯入擔任幹部記錄 { get { return "K12.Student.Student.Ribbon0168"; } }
-        //public static string 匯入擔任幹部記錄_國中 { get { return "JHSchool.Student.Ribbon0168"; } }
+        public static string 匯入擔任幹部記錄_國中 { get { return "JHSchool.Student.Ribbon0168"; } }
 
         public static string 幹部名稱管理 { get { return "K12.class.TheCadre.Report00070.5"; } }
-        //public static string 幹部名稱管理_國中 { get { return "Behavior.TheCadre.Report00070.5"; } }
+        public static string 幹部名稱管理_國中 { get { return "Behavior.TheCadre.Report00070.5"; } }
 
         public static string 學校幹部登錄 { get { return "K12.class.TheCadre.Report00080"; } }
-        //public static string 學校幹部登錄_國中 { get { return "Behavior.TheCadre.Report00080"; } }
+        public static string 學校幹部登錄_國中 { get { return "Behavior.TheCadre.Report00080"; } }
 
         public static string 幹部批次修改 { get { return "8E64AE0D-1D86-465D-BE4D-16B7C3AFAB68"; } }
 
@@ -64,8 +64,12 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[學校幹部登錄].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[學校幹部登錄_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[學校幹部登錄_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                return check1;
             }
         }
 
@@ -74,8 +78,13 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[幹部名稱管理].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[幹部名稱管理_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[幹部名稱管理_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                
+                return check1;
             }
         }
 
@@ -84,8 +93,13 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[幹部記錄].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[幹部記錄_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[幹部記錄_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                
+                return check1;
             }
         }
 
@@ -94,8 +108,12 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[學生幹部證明單].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[學生幹部證明單_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[學生幹部證明單_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                return check1;
             }
         }
 
@@ -104,8 +122,12 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[班級幹部登錄].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[班級幹部登錄_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[班級幹部登錄_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                return check1;
             }
         }
 
@@ -114,8 +136,12 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[匯出擔任幹部記錄].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[匯出擔任幹部記錄_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[匯出擔任幹部記錄_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                return check1;
             }
         }
 
@@ -124,8 +150,12 @@ namespace K12.Behavior.TheCadre
             get
             {
                 bool check1 = FISCA.Permission.UserAcl.Current[匯入擔任幹部記錄].Executable;
-                //bool check2 = FISCA.Permission.UserAcl.Current[匯入擔任幹部記錄_國中].Executable;
-                return check1;// || check2;
+                bool check2 = FISCA.Permission.UserAcl.Current[匯入擔任幹部記錄_國中].Executable;
+                if (check2)
+                {
+                    check1 = check2;
+                }
+                return check1;
             }
         }
 
