33f45c67a3cb5d75ac0143ac475fcf8d5f607772
JeanWu <jean.wu@ischool.com.tw>
2019-03-26 13:53:18 +0800

    孟樺_之前漏掉 補上

---
+++ K12.Behavior.TheCadre/DAO/LogDao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace K12.Behavior.TheCadre.DAO
+{
+    //為了log 登入使用
+    class LogDao
+    {
+        public string SchoolYear { get; set; }
+        public string Semester { get; set; }
+        public string OccurDate { get; set; }
+        public string Reason { get; set; }
+        public string Retail { get; set; }
+        public string StudentID { get; set; }
+        public string CadreName { get; set; }
+        public string ClassName { get; set; }
+        public string SeatNo { get; set; }
+        public string StudentName { get; set; }
+        public string Merit_flag { get; set; }
+        public string RegisterDate { get; set; }
+        public int  A { get; set; }
+        public int  B { get; set; }
+        public int  C { get; set; }
+        
+     
+
+
+
+       
+    }
+}
